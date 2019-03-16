121b139a8d3616a8644f087f7dab2f966fd9782d
professor huang <954385120@qq.com>
2019-03-17 00:28:29 +0800

    add status diff

--- git-command.sql
+++ git-command.sql
@@ -1,7 +1,17 @@
 打开到指定文件：在属性处更改打开位置。
 
-pwd 		-- print working directory 显示档期当前目录
+pwd 				-- print working directory 显示档期当前目录
 
-ls -a	  	--	ls 工作目录下文件以及子目录 
-			--	-a 所有文件以及目录（除隐藏外） 
+ls -a	  			--	ls 工作目录下文件以及子目录 
+					--	-a 所有文件以及目录（除隐藏外） 
+					
+git add fileName	-- 	submit file to repository
+
+git submit -m "describe" -- submit file to repository
+
+git submit -a "describe"
+
+git status 				-- show file is change
+
+git diff fileName		-- show file difference
 			
